0234adc03935c630f4340601757706005bc6d735
tashi1118 <tech.tashi1118.1991@gmail.com>
2021-05-30 23:54:44 +0900

    bookテーブル

---
+++ db/migrate/20210530144842_create_books.rb
@@ -0,0 +1,11 @@
+class CreateBooks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :books do |t|
+      t.string :title,       null: false, default: ""
+      t.string :description, null: false, default: ""
+      t.string :url,         null: false, default: ""
+
+      t.timestamps
+    end
+  end
+end
