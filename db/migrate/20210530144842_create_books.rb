b12b717b735bd49a95c8b184a46bcb4fc236cedc
tashi1118 <tech.tashi1118.1991@gmail.com>
2021-06-01 23:28:28 +0900

    urlはnullableにする

--- db/migrate/20210530144842_create_books.rb
+++ db/migrate/20210530144842_create_books.rb
@@ -3,7 +3,7 @@ class CreateBooks < ActiveRecord::Migration[6.1]
     create_table :books do |t|
       t.string :title,       null: false, default: ""
       t.string :description, null: false, default: ""
-      t.string :url,         null: false, default: ""
+      t.string :url,         null: true,  default: ""
 
       t.timestamps
     end
